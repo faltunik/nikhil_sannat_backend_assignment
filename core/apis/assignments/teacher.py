ebcfc8f853c7545970dfd83f7bfc0c00458b5b49
faltunik <niksanat8@gmail.com>
2022-08-13 18:53:12 +0530

    added the api for grading the assignment

--- core/apis/assignments/teacher.py
+++ core/apis/assignments/teacher.py
@@ -4,7 +4,7 @@ from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
 
-from .schema import AssignmentSchema, AssignmentSubmitSchema
+from .schema import AssignmentSchema, AssignmentGradeSchema
 teacher_assignments_resources = Blueprint('teacher_assignments_resources', __name__)
 
 @teacher_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
@@ -14,3 +14,21 @@ def list_assignments(p):
     students_assignments = Assignment.get_assignments_submitted_to_student(p.student_id)
     students_assignments_dump = AssignmentSchema().dump(students_assignments, many=True)
     return APIResponse.respond(data=students_assignments_dump)
+
+
+#API for grading the assignment
+@teacher_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.auth_principal
+def grade_assignment(p, incoming_payload):
+    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    graded_assignment = Assignment.grade_student_assignment(
+        _id = grade_assignment_payload.id,
+        _grade=grade_assignment_payload.grade,
+        principal=p
+    )
+
+    db.session.commit()
+    graded_assignment=AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(graded_assignment)
--- core/models/assignments.py
+++ core/models/assignments.py
@@ -83,7 +83,7 @@ class Assignment(db.Model):
         return cls.filter(cls.teacher_id == teacher_id).all()
 
     @classmethod
-    def grade_student(cls, _id, _grade, principal: Principal):
+    def grade_student_assignment(cls, _id, _grade, principal: Principal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'Not submitted to teacher')   
